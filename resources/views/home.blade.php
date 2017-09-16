033aa91c6bc61969bc2ec1549cfec3a90c523356
acubaniti <acubaniti@gmail.com>
2017-09-16 13:25:55 +0300

    second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,56 +26,7 @@
          </div>
     </div> -->
  <script type="text/javascript">
-      
-
-        $(function() {
-  
-                var socket = io.connect('http://192.168.0.10:3000');
-                // injection
-                var $messageForm  = $('#messageForm');
-                var $message  = $('#message');
-                var $chat  = $('#chat'); 
-                var $messageArea = $('#messageArea'); 
-                var $users = $('#users');
-                // user config
-                var $username = '{{ Auth::user()->name}}';   
-                socket.emit('new user', $username);
-                console.log($username);
-
-                $messageForm.submit(function(e){
-                  e.preventDefault(); 
-                  socket.emit('send message', $message.val());
-                  $message.val('');
-                });
-                socket.on('new message', function(data){
-                  $chat.append('<div class="row single_message"><div class="user_data"><img class="avatar"  src="https://cdn.shopify.com/s/files/1/0843/2781/products/RapMask_Mark-Zuckerberg_Excited_9Width_Standard_grande.png"/><div class="user_post">'+ data.user +'<\/div><div class="secondary_data"><ul><li>Age: 26</li><li>Location: Bucharest</li><li>Sex: Male</li></ul></div></div><div class="transmission">' + data.msg + '<\/div><\/div>');
-                  playAudio(); 
-                });
- 
-                socket.on('get users', function(data){
-                  var usersOnline = ''; 
-                  for(i = 0; i < data.length; i++) {
-                    usersOnline += '<li class="list-group-item user_is_online"><img class="avatar" src="https://cdn.shopify.com/s/files/1/0843/2781/products/RapMask_Mark-Zuckerberg_Excited_9Width_Standard_grande.png"/>'+ data[i] +'<\/li>'; 
-                  }
    
-                  $users.html(usersOnline);
-                });
-               
-               });
-  // Chat audio pop    
-   var ding = document.getElementById("myAudio");
-   function playAudio() { 
-   ding.play(); 
-   };
-
-$(document).keypress(function(e) {
-  if(e.which == 13) {
-    // enter pressed
-    console.log('O sugi in kek');
-    $('#send_message').submit();
-  }
-});
-
 
  </script>
 @endsection
